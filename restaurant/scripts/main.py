57a0e33e840f6cab031cced40f1aa42b79c55279
Adam <a@b.c>
2019-07-16 15:33:25 +0200

    Setup dependencies

--- restaurant/scripts/main.py
+++ restaurant/scripts/main.py
@@ -1,6 +1,17 @@
-
-
-
+from restaurant.cash_desk import CashDesk
+from restaurant.kitchen import Kitchen
+from restaurant.give_away import GiveAway
+from restaurant.manager import Manager
 
 if __name__ == '__main__':
-    pass
+    cash_desk = CashDesk(None)
+    kitchen = Kitchen(None)
+    give_away = GiveAway(None)
+
+    manager = Manager(cash_desk, kitchen, give_away)
+
+    cash_desk.manager = manager
+    kitchen.manager = manager
+    give_away.manager = manager
+
+    cash_desk.new_order("Burger")
